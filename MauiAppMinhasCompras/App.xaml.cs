9476928bcb7e0b4c3e2dabf6d5272e2adebf125c
Thiago Abreu <tabreu.br@gmail.com>
2025-08-19 20:48:18 -0300

    Criação da tela NovoProduto e seus botões, e alterações de interface

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper db;
+   
+
+        public static SQLiteDatabaseHelper Database
+        {
+            get
+            {
+                if (db == null)
+                {
+                   string path = Path.Combine(
+                       Environment.GetFolderPath(
+                           Environment.SpecialFolder.LocalApplicationData),
+                       "banco_sqlite_compras.db3");
+
+                   db = new SQLiteDatabaseHelper(path);
+                }
+
+                return db;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,9 +1,38 @@
+using System.Threading.Tasks;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
 {
-	public NovoProduto()
-	{
-		InitializeComponent();
-	}
+    public NovoProduto()
+    {
+        InitializeComponent();
+    }
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+
+            };
+
+            await App.Database.Insert(p);
+            await DisplayAlert("Sucesso", "Produto adicionado com sucesso!", "OK!");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK!");
+        }
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
 }
