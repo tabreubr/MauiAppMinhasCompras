f8d80837aab0a1cb23a3d46ff809a45a6e898824
Thiago Abreu <tabreu.br@gmail.com>
2025-08-29 18:28:49 -0300

    SearchBar com TextChanged, ObservableCollection, Filtro em tempo real, Atualização automática da UI

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,20 +1,70 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
+    public ListaProduto()
+    {
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = lista;
+    }
+
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Database.GetAll();
+
+        tmp.ForEach(i => lista.Add(i));
+    }
 
     private void ToolbarItem_Clicked(object sender, EventArgs e)
     {
-		try
-		{
-			Navigation.PushAsync(new Views.NovoProduto());
-		} catch (Exception ex)
-		{
-			DisplayAlert("Ops", ex.Message, "OK!");
-		}
+        try
+        {
+            Navigation.PushAsync(new Views.NovoProduto());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops", ex.Message, "OK!");
+        }
     }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        lista.Clear();
+
+        List<Produto> tmp = await App.Database.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = lista.Sum(i => i.Total);
+
+        string msg = $"O total da sua compra é {soma:C2}";
+
+        DisplayAlert("Total dos Produtos:", msg, "OK");
+
+    }
+
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        var menuItem = sender as MenuItem;
+        var produto = menuItem?.BindingContext as Produto;
+
+        if (produto != null)
+        {
+            await App.Database.Delete(produto.Id);
+            lista.Remove(produto);
+            await DisplayAlert("Sucesso", "Produto removido!", "OK");
+        }
+    }
+
 }
